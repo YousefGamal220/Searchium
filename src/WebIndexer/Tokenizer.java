34d86cc8b9199d281f7e388ed9301a8e4ae16cad
Yousef Gamal <yousefgamal295@outlook.com>
2021-05-08 02:14:09 +0200

    Stemmer V1

--- src/WebIndexer/Tokenizer.java
+++ src/WebIndexer/Tokenizer.java
@@ -5,21 +5,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * The type Tokenizer.
+ */
 public class Tokenizer {
+    /**
+     * Tokenize word list.
+     *
+     * @param sentence the sentence
+     * @return the list
+     */
     public static List<String> tokenizeWord(String sentence)
     {
-        /*
+        /**
          * This Function Returns the tokenization of a given sentence<String>
          * it takes String Parameter sentence
          * And Returns List of Strings the Tokens
 
          * Example: Given Sentence "Welcome to Searchium" as input
          * Output: ["Welcome", "to", "Searchium"]
-         * */
+         */
+
         return new ArrayList<String>(Arrays.asList(sentence.split(" ")));
     }
 
-    /*
+    /**
     * The Following two functions are overrided
     * They do the same functionality
     * but the difference in the input
@@ -29,8 +39,14 @@ public class Tokenizer {
     * The Second Function takes parameter Full Corpus and regex splitter for example \n OR <br>
     * The First thing it do is to convert the corpus to the form of the first function as List of Strings,
     * then it do the same functionality as the first mentioned function
-    * */
+     */
 
+    /**
+     * Tokenize corpus list.
+     *
+     * @param corpus the corpus
+     * @return the list
+     */
     public static List<List<String>> tokenizeCorpus(List<String> corpus)
     {
 
@@ -44,7 +60,13 @@ public class Tokenizer {
     }
 
 
-
+    /**
+     * Tokenize corpus list.
+     *
+     * @param corpus   the corpus
+     * @param splitter the splitter
+     * @return the list tokenizedCorpus
+     */
     public static List<List<String>> tokenizeCorpus(String corpus, String splitter)
     {
         List<String> sentences = new ArrayList<String>(Arrays.asList(corpus.split(splitter)));
