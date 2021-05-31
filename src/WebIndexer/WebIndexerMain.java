8accc3fe8092abe9bf61c6b99318764a7beb978f
yahia3200 <yahiazakaria3200@gmail.com>
2021-05-31 23:50:30 +0200

    remove stoping words

--- src/WebIndexer/WebIndexerMain.java
+++ src/WebIndexer/WebIndexerMain.java
@@ -3,7 +3,7 @@ package WebIndexer;
 /**
  * WebIndexer Stages
  * 1- Tokenizer (Gamal) Done
- * 2- Remove Stopping Words [Optional: To make it Smarter] (Yahia)
+ * 2- Remove Stopping Words [Optional: To make it Smarter] (Yahia) Done
  * 3- Remove HTML tags (Yahia) Done
  * 4- Stemming [Smart Stemmer] (Gamal) Done
  * 5- Indexing and TF-IDF (Yahia and Gamal)
