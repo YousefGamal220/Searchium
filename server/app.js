992e672b3a2614bde237b714eba49caddbf83d63
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-06-07 22:12:39 +0200

    Server: added snippets to the response

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const Word = require('./models/Word');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 require('dotenv').config();
 require('./mongo');
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.get("/search", paginatedResults(), async (req, res) => {
     res.header("Content-Type", 'application/json');
-    res.status(200).send(JSON.stringify(res.paginatedResults, null, 4));
+    res.status(200).send(JSON.stringify(res.paginatedResults, null, 1));
 });
 
 function paginatedResults() {
@@ -32,13 +32,23 @@ function paginatedResults() {
         try {
             const { IDF, pages } = await Word.findOne({ word }).select('IDF pages -_id');
             const slicedPages = pages.slice(startIndex, endIndex);
-            slicedPages.sort((a, b) => { return (b.tf * IDF) - (a.tf * IDF) })
+            slicedPages.sort((a, b) => { return (b.tf * IDF) - (a.tf * IDF) });
             let index = 0;
             for (let page of slicedPages) {
                 const { title, content } = await CrawledPage.findOne({ url: page.url }).select('title content -_id');
                 delete page.tf;
                 slicedPages[index++] = { ...page, title, content };
             }
+            slicedPages.forEach(page => {
+                const wordIndex = page.content.toLowerCase().indexOf(word);
+                let startIndex = wordIndex;
+                let endIndex = wordIndex;
+                while (page.content[startIndex] != ". " && (page.content[startIndex] != " " || wordIndex - startIndex < 100)) startIndex--;
+                while (page.content[endIndex] != ". " && (page.content[endIndex] != " " || endIndex - wordIndex < word.length + 100)) endIndex++;
+                page.snippet = page.content.slice(startIndex + 1, endIndex);
+                delete page.content;
+            });
+
             if (endIndex < pages.length) {
                 results.next = page + 1;
             }
